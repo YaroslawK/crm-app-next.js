91bfdc3ac545042a79d4898408adaf7ff16e1885
Yaroslaw <xxjokerxx933@gmail.com>
2024-10-07 17:30:52 +0300

    init Tailwind CSS

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import css from './active-label.module.css';
+import React from 'react';
 
-
-export interface ActiveLabelProps{
-    children: React.ReactNode;
+export enum Status {
+  Active = 'active',
+  NotActive = 'notActive',
+  Pending = 'pending',
+  Suspended = 'suspended',
 }
 
-export const ActiveLabel = ({children}: ActiveLabelProps) => {
-    return <span className={css.label}>{children}</span>
+export interface StatusLabelProps {
+  children: React.ReactNode;
+  status: Status;
 }
+
+export const StatusLabel = ({ children, status }: StatusLabelProps) => {
+  return (
+    <div
+      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
+        (status === Status.Active && 'text-green-500 bg-green-100') ||
+        (status === Status.NotActive && 'text-red-500 bg-green-100') ||
+        (status === Status.Pending && 'text-orange-500 bg-green-100') ||
+          (status === Status.Suspended && 'text-blue-500 bg-green-100') ||
+              ""
+      }`}>
+          <div className="w-1 h-1 mr-2 rounded-full bg-current"></div>
+      {children}
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { ActiveLabel } from "./components/active-label";
+import { Status, StatusLabel } from "./components/active-label";
 
 export default function Home() {
   return <div>
-      <h1>Home page</h1>
-      <ActiveLabel>Active</ActiveLabel>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
         </div>
  
 }
